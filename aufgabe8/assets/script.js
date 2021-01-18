19b6dce4945ff5a022f891edba267ae191bf2b25
NinaHecht <64583340+NinaHecht@users.noreply.github.com>
2021-01-18 16:11:29 +0100

    a8 full version

--- aufgabe8/assets/script.js
+++ aufgabe8/assets/script.js
@@ -2,61 +2,56 @@ const video = document.getElementById('360_video');
 var mailContent= "";
 
 var Artyom = new Artyom();
+var firststart=true;
 
 function startContinuousArtyom() {
     Artyom.fatality();
     setTimeout(function () {
         Artyom.initialize({
-            lang: "en-GB",
+            lang: "de-DE",
             continuous: true,
             listen: true,
             interimResults: false,
             debug: true,
         }).then(function () {
             console.log('started');
-            Artyom.addCommands(commands);
+            Artyom.addCommands(play);
+            Artyom.addCommands(mail);
+            if(firststart){
+                video.play();
+                firststart = false;
+            }
         });
     }, 250);
 }
 
-var MailContent = Artyom.newDictation({
-    continuous: true, 
-    lang: "en-GB",
-    onResult:function(text){
-        console.log(text);
-        if(text){
-            mailContent = text;
-        }
-    },
-    onStart:function(){
-        console.log("Dictation started by the user");
-        document.getElementById('content').setAttribute('value', 'Jetzt diktieren (10sek)');
-    },
-    onEnd:function(text){
-        document.getElementById('content').setAttribute('value', mailContent);
-        console.log("Dictation ended");
-        startContinuousArtyom();
-    }
-});
-
 function mailWindow() {
+    Artyom.emptyCommands();
+    Artyom.addCommands(play);
     document.getElementById('mail').setAttribute('visible', 'true');
-    Artyom.fatality();
-    MailContent.start();
-    setTimeout(function(){
-        MailContent.stop();
-    }, 10000)
+
+    Artyom.redirectRecognizedTextOutput(function(recognized,isFinal){
+        if(isFinal){
+            console.log("Final recognized text: " + recognized);
+            document.getElementById('content').setAttribute('value', recognized);
+            Artyom.fatality();
+            startContinuousArtyom();
+            Artyom.addCommands(mail);
+        }
+    });
 }
 
 startContinuousArtyom();
 
-var commands = [
+var play = [
     {
         smart:true, 
         indexes:["* play", "play *"],
         action:function(i,wildcard){
             console.log("play");
             video.play();
+            document.getElementById('play').setAttribute('visible', 'true');
+            setTimeout(function(){document.getElementById('play').setAttribute('visible', 'false');}, 3000)
         }
     },
     {
@@ -64,6 +59,8 @@ var commands = [
         action:function(){
             console.log("play");
             video.play();
+            document.getElementById('play').setAttribute('visible', 'true');
+            setTimeout(function(){document.getElementById('play').setAttribute('visible', 'false');}, 3000)
         }
     },
     {
@@ -72,6 +69,8 @@ var commands = [
         action:function(i,wildcard){
             console.log("pause");
             video.pause();
+            document.getElementById('pause').setAttribute('visible', 'true');
+            setTimeout(function(){document.getElementById('pause').setAttribute('visible', 'false');}, 3000)
         }
     },
     {
@@ -79,8 +78,13 @@ var commands = [
         action:function(){
             console.log("pause");
             video.pause();
+            document.getElementById('pause').setAttribute('visible', 'true');
+            setTimeout(function(){document.getElementById('pause').setAttribute('visible', 'false');}, 3000)
         }
-    },
+    }
+]; 
+
+var mail = [
     {
         smart:true, 
         indexes:["* E-Mail", "E-Mail *", "* Mail", "Mail *"],
@@ -97,7 +101,7 @@ var commands = [
         }
     },
     {
-        indexes:["Send", "Send E-Mail", "Send Mail"],
+        indexes:["Send", "Send E-Mail", "Send Mail", "Senden", "Absenden"],
         action:function(){
             console.log("send");
             document.getElementById('content').setAttribute('value', '');
