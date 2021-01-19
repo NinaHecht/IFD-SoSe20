304c3e858b69f02990340be4c38d94382050ca7f
NinaHecht <64583340+NinaHecht@users.noreply.github.com>
2021-01-19 17:28:37 +0100

    removed some inline styles and scripts

---
+++ aufgabe3/res/script.js
@@ -0,0 +1,43 @@
+$("#menu-toggle").on("click", function() {
+    $("#menu-sub-nav").slideToggle();
+    $("#menu-toggle").toggleClass('menu-open');
+});
+$(document).ready(function() {
+    $("#tabs").tabs();
+});
+$(".accordion").on("click", function(){
+    $(this).toggleClass("open");
+    let id = $(this).attr("id");
+    $("#" + id + "-content").slideToggle();
+});
+$(".fake-link").on("click", function(event){
+    event.preventDefault();
+});
+$("#search-link").on("click", function(event){
+    event.preventDefault();
+    $("#search-input").addClass("open");
+});
+
+$("#participate").on("click", function() {
+    $("#participant").fadeToggle();
+    $("#participate").toggleClass('true');
+});
+
+$(".remove-participant").on("click", function() {
+    $(this).fadeToggle();
+});
+$(".modal-trigger").on("click", function() {
+    $(".accordion.open ~ .accordion-content").slideUp(1);
+    $(".accordion.open").removeClass("open");
+    if(!$("body").hasClass("modal-open")) {
+        let id = $(this).attr("id");
+        $("#" + id + "-modal").slideDown().addClass("open");
+        $("body").addClass("modal-open");
+    }
+});
+$(".modal-close").on("click", function() {
+    if($("body").hasClass("modal-open")) {
+        $(".modal.open").slideUp();
+        $("body").removeClass("modal-open");
+    }
+});
